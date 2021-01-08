5ff7fdf9d3974d1d3574d3f9aa1cffae3b555d52
steve <kkingya@gmail.com>
2021-01-08 17:39:41 -0500

    editted

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const axios = require("axios");
 const converter = require("json-2-csv");
 const fs = require("fs");
 
-const player = "faker";
-const url =
-  "https://www.rotowire.com/esports/ajax/player-page-data.php?id=496&game=2";
+const players = ["faker", "deft", "showmaker"];
+const id = [496, 473, 3704];
 
-axios.get(`${url}`).then((res) => {
-  converter.json2csv(res.data.gamelog.body, (err, csv) => {
-    if (err) {
-      throw err;
-    }
-    fs.writeFileSync(`${player}.csv`, csv);
-  });
-});
+for (let i = 0; i < players.length; i++) {
+  axios
+    .get(
+      `https://www.rotowire.com/esports/ajax/player-page-data.php?id=${id[i]}&game=2`
+    )
+    .then((res) => {
+      converter.json2csv(res.data.gamelog.body, (err, csv) => {
+        if (err) {
+          throw err;
+        }
+        fs.writeFileSync(`${players[i]}.csv`, csv);
+      });
+    });
+}
